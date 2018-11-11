768c2d9a672b0a4ee03cbaa4b8284a0a37c0f200
m <m@bitsnbites.eu>
2018-11-11 12:07:35 +0100

    [MRISC32] Add MRISC32 support in BFD

--- bfd/bfd-in2.h
+++ bfd/bfd-in2.h
@@ -1932,6 +1932,8 @@ enum bfd_architecture
 #define bfd_mach_ck807         6
 #define bfd_mach_ck810         7
 #define bfd_mach_ck860         8
+  bfd_arch_mrisc32,  /* MRISC32.  */
+#define bfd_mach_mrisc32       1
   bfd_arch_last
   };
 
@@ -6263,6 +6265,23 @@ assembler and not (currently) written to any object files.  */
 
 /* S12Z relocations.  */
   BFD_RELOC_S12Z_OPR,
+
+/* MRISC32 Relocations.  */
+  BFD_RELOC_MRISC32_PCREL_18X4,
+  BFD_RELOC_MRISC32_PCREL_21X4,
+  BFD_RELOC_MRISC32_PCREL_HI21,
+  BFD_RELOC_MRISC32_PCREL_LO11,
+  BFD_RELOC_MRISC32_PCREL_LO9X4,
+  BFD_RELOC_MRISC32_I14,
+  BFD_RELOC_MRISC32_IHI14,
+  BFD_RELOC_MRISC32_I15,
+  BFD_RELOC_MRISC32_I20,
+  BFD_RELOC_MRISC32_IHI20,
+  BFD_RELOC_MRISC32_IHI19,
+  BFD_RELOC_MRISC32_ILO13,
+  BFD_RELOC_MRISC32_ILO11X4,
+  BFD_RELOC_MRISC32_I21X4,
+
   BFD_RELOC_UNUSED };
 
 typedef enum bfd_reloc_code_real bfd_reloc_code_real_type;
