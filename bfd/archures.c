75bf5f839f9ef5e49992e83a3c7cc884d86ed95e
m <m@bitsnbites.eu>
2018-11-11 12:07:35 +0100

    [MRISC32] Add MRISC32 support in BFD

--- bfd/archures.c
+++ bfd/archures.c
@@ -566,6 +566,8 @@ DESCRIPTION
 .#define bfd_mach_amdgcn_gfx1030 0x036
 .#define bfd_mach_amdgcn_gfx1031 0x037
 .#define bfd_mach_amdgcn_gfx1032 0x038
+.  bfd_arch_mrisc32,  {* MRISC32.  *}
+.#define bfd_mach_mrisc32	1
 .  bfd_arch_last
 .  };
 */
@@ -664,6 +666,7 @@ extern const bfd_arch_info_type bfd_mn10200_arch;
 extern const bfd_arch_info_type bfd_mn10300_arch;
 extern const bfd_arch_info_type bfd_moxie_arch;
 extern const bfd_arch_info_type bfd_ft32_arch;
+extern const bfd_arch_info_type bfd_mrisc32_arch;
 extern const bfd_arch_info_type bfd_msp430_arch;
 extern const bfd_arch_info_type bfd_mt_arch;
 extern const bfd_arch_info_type bfd_nds32_arch;
@@ -753,6 +756,7 @@ static const bfd_arch_info_type * const bfd_archures_list[] =
     &bfd_mn10300_arch,
     &bfd_moxie_arch,
     &bfd_ft32_arch,
+    &bfd_mrisc32_arch,
     &bfd_msp430_arch,
     &bfd_mt_arch,
     &bfd_nds32_arch,
